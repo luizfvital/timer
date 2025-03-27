f9f56ac4ca7e051d4281be4ecd935774e2e6ebef
Luiz Fernando Vital <luizfvital@gmail.com>
2025-03-27 19:09:29 -0300

    add button component

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+interface ButtonContainerProps {
+  variant: 'primary' | 'secondary' | 'danger' | 'success'
+}
+
+export const ButtonContainer = styled.button`
+  
+`;
